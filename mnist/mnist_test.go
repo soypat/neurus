8d665a895281ad8e9e3bb9ab0fb354f40bc2f6c2
soypat <graded.sp@gmail.com>
2022-10-29 13:04:44 -0300

    fix MNIST database

--- mnist/mnist_test.go
+++ mnist/mnist_test.go
@@ -1,11 +1,16 @@
 package mnist
 
 import (
+	"bytes"
 	"compress/gzip"
-	"encoding/binary"
+	"encoding/csv"
+	"image/png"
 	"io"
+	"os"
+	"strconv"
 	"testing"
 	"time"
+	"unsafe"
 )
 
 var (
@@ -14,6 +19,7 @@ var (
 )
 
 func TestImageLabels(t *testing.T) {
+	totalMislabels := 0
 	for _, test := range []struct {
 		Label  string
 		Images []Image
@@ -25,15 +31,22 @@ func TestImageLabels(t *testing.T) {
 		images := test.Images
 		t.Run("test image numbers:"+test.Label, func(t *testing.T) {
 			for i := range images {
-				if images[i].Num > 9 {
+				if images[i].Num > 9 && totalMislabels == 0 {
+					totalMislabels++
 					t.Errorf("image-%d labelled as %d", i, images[i].Num)
+				} else if images[i].Num > 9 {
+					totalMislabels++
 				}
+			}
+			if totalMislabels > 1 {
+				t.Errorf("%d/%d mislabels in image set", totalMislabels, len(images))
 			}
 		})
 	}
 }
 
 func TestImage64Labels(t *testing.T) {
+	totalMislabels := 0
 	for _, test := range []struct {
 		Label  string
 		Images []Image64
@@ -45,9 +58,15 @@ func TestImage64Labels(t *testing.T) {
 		images := test.Images
 		t.Run("test image numbers:"+test.Label, func(t *testing.T) {
 			for i := range images {
-				if images[i].Num > 9 {
+				if images[i].Num > 9 && totalMislabels == 0 {
+					totalMislabels++
 					t.Errorf("image-%d labelled as %d", i, images[i].Num)
+				} else if images[i].Num > 9 {
+					totalMislabels++
 				}
+			}
+			if totalMislabels > 1 {
+				t.Errorf("%d/%d mislabels in image set", totalMislabels, len(images))
 			}
 		})
 	}
@@ -68,7 +87,7 @@ func TestImage64Data(t *testing.T) {
 				for j, v := range images[i].Data {
 					if v < 0 || v > 1 {
 						t.Errorf("image-%d corrupt as %f starting at index %d", i, v, j)
-						break // If one value corrupt, multiple others probably too
+						return // If one value corrupt, multiple others probably too
 					}
 				}
 			}
@@ -91,7 +110,7 @@ func TestImageData(t *testing.T) {
 				for j, v := range images[i].Data {
 					if v < 0 || v > 1 {
 						t.Errorf("image-%d corrupt as %f starting at index %d", i, v, j)
-						break // If one value corrupt, multiple others probably too
+						return // If one value corrupt, multiple others probably too
 					}
 				}
 			}
@@ -99,10 +118,72 @@ func TestImageData(t *testing.T) {
 	}
 }
 
+// Used to generate the MNIST database
+func generate(t *testing.T) {
+	files := []string{"test", "validation", "training"}
+	for _, label := range files {
+		csvfile, err := os.Open(label + ".csv")
+		if err != nil {
+			t.Fatal(err)
+		}
+		images := unmarshal_csv(csvfile)
+		gzfilename := label + ".bin.gz"
+		gz, err := os.Create(gzfilename)
+		if err != nil {
+			t.Fatal(err)
+		}
+		gzDump(gz, images)
+		gz.Close()
+		gz, err = os.Open(gzfilename)
+		if err != nil {
+			t.Fatal(err)
+		}
+		images = gzLoad(gz)
+		pngfile, _ := os.Create(label + "-1000.png")
+		png.Encode(pngfile, &images[1000])
+	}
+}
+
 func gzDump(w io.Writer, images []Image) {
 	gzw := gzip.NewWriter(w)
 	gzw.ModTime = time.Now()
 	gzw.Header.Comment = "binary images encoded as [28*28]float32; [1]uint8"
-	binary.Write(gzw, binary.LittleEndian, images)
+	buf := unsafe.Slice((*byte)(unsafe.Pointer(&images[0])), len(images)*int(unsafe.Sizeof(Image{})))
+	_, err := io.Copy(gzw, bytes.NewReader(buf))
+	if err != nil {
+		panic(err)
+	}
 	gzw.Close()
+}
+
+func unmarshal_csv(r io.Reader) (images []Image) {
+	c := csv.NewReader(r)
+	c.FieldsPerRecord = PixelCount + 2
+	c.ReuseRecord = true
+
+	for {
+		var img Image
+		record, err := c.Read()
+		if err != nil {
+			return images
+		}
+
+		num, err := strconv.ParseUint(record[0], 10, 8)
+		if err != nil || num > 9 {
+			panic("bad number format")
+		}
+		img.Num = uint8(num)
+		for dataIdx, v := range record[1:] {
+			if v == "" {
+				continue
+			}
+			j, i := dataIdx/imgSize, dataIdx%imgSize
+			f, err := strconv.ParseFloat(v, 32)
+			if err != nil {
+				panic("bad image pixel")
+			}
+			img.Data[i*imgSize+j] = float32(f)
+		}
+		images = append(images, img)
+	}
 }
